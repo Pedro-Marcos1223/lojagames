b3c4d37e087f929dace32e7d5e08a3a10c9d9869
Jônatas <OliveiraJonatas@Outlook.com.br>
2022-02-23 10:48:06 -0300

    @jonatasoliveiradev [Task: criação da tabela produto]

--- lojagames/src/main/java/org/generation/lojagames/lojagames/model/Produto.java
+++ lojagames/src/main/java/org/generation/lojagames/lojagames/model/Produto.java
@@ -1,5 +1,61 @@
 package org.generation.lojagames.lojagames.model;
 
-public class Produto {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 1, max = 50)
+	private String nome;
+	
+	@NotNull
+	@Size(min = 1, max = 30)
+	private String fabricante;
+	
+	@NotNull
+	private double valor;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
 }
